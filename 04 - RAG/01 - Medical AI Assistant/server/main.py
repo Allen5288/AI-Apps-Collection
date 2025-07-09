9e451150c56a5e94a9f0de7e56a915fcca243786
Allen5288 <yuanxin_xie_ee@163.com>
2025-07-09 17:27:06 +1000

    feat: medical ai assistant add routers

--- 04 - RAG/01 - Medical AI Assistant/server/main.py
+++ 04 - RAG/01 - Medical AI Assistant/server/main.py
@@ -1,8 +1,14 @@
-from  fastapi  import FastAPI
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from middlewares.exception_handlers import catch_exception_middleware
+from routes.upload_pdfs import router as upload_router
+from routes.ask_question import router as ask_router
 
-app=FastAPI(title="Medical Assistant API",description="API for Medical AI Assistant",version="1.0.0")
+app = FastAPI(
+    title="Medical Assistant API",
+    description="API for Medical AI Assistant",
+    version="1.0.0",
+)
 
 # CORS configuration
 app.add_middleware(
@@ -18,4 +24,7 @@ app.middleware("http")(catch_exception_middleware)
 
 # routers
 # 1. upload pdf document
+app.include_router(upload_router)
+
 # 2. asking query
+app.include_router(ask_router)
